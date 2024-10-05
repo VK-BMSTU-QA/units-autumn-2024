4513bf66622aa12949e08ace66428532b13abbcb
Artem <List.kedra.79@gmail.com>
2024-10-05 23:50:59 +0300

    fix

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -3,7 +3,7 @@ import sys
 import os
 import math
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from calculator import Calculator
+from src.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
